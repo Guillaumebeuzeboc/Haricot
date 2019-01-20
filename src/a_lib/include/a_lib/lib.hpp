c1e49e89d80e30b1016fd8032f145db6db9ba9ac
Guillaume beuzeboc <beuzeboc@magazino.eu>
2019-01-20 02:46:14 +0100

    feat(QT5CustomPlot): qt5 custom plot lib build + example of usage

--- src/a_lib/include/a_lib/lib.hpp
+++
@@ -1,5 +0,0 @@
-#include "boost/date_time/gregorian/gregorian.hpp"
-#include <iostream>
-#include <string>
-
-void liba_function();
--- src/z_lib/include/z_lib/lib.hpp
+++
@@ -1,6 +0,0 @@
-#include "boost/date_time/gregorian/gregorian.hpp"
-#include <iostream>
-#include <string>
-#include <Eigen/Dense>
-
-void libz_function();

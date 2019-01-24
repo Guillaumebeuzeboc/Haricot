62bc1b804afa3477a95eeec2da42aa8c6f4f8f47
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2019-01-24 16:17:06 +0100

    feat(python): first step of the python install

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,3 +1,37 @@
 
-macro(prepare_python)
+macro(PREPARE_PYTHON)
+    set(CMAKE_PYTHON_BUILD "${ROOT_DIR}/build/python/lib/python3.5/site-packages")
+    set(CMAKE_PYTHON_INSTALL "${ROOT_DIR}/install/lib/python3.5/site-packages")
+    file(MAKE_DIRECTORY ${CMAKE_PYTHON_BUILD})
+    file(MAKE_DIRECTORY ${CMAKE_PYTHON_INSTALL})
+endmacro()
+
+
+macro(SETUP_PYTHON_PKG)
+     set(ENV{PYTHONPATH} "$ENV{PYTHONPATH}:${CMAKE_PYTHON_BUILD}")
+     file(MAKE_DIRECTORY ${CMAKE_PYTHON_BUILD}/${PROJECT_NAME})
+     set(INIT_PKG_BUILD ${CMAKE_PYTHON_BUILD}/${PROJECT_NAME}/__init__.py)
+     set(PYTHON_SOURCE_PATH "${PROJECT_SOURCE_DIR}/src")
+     configure_file(${ROOT_DIR}/cmake/__init__.py.in ${INIT_PKG_BUILD})
+
+     install(CODE "
+            set(cmd \"PYTHONPATH=${CMAKE_PYTHON_INSTALL}:$ENV{PYTHONPATH} && /usr/bin/python3 ${PROJECT_SOURCE_DIR}/setup.py install --prefix=${ROOT_DIR}/install\")
+            execute_process(COMMAND bash -c \${cmd1}
+                            WORKING_DIRECTORY ${ROOT_DIR}/install
+                            OUTPUT_VARIABLE output
+                            RESULT_VARIABLE result)
+            message(\"\${output}!!!\")
+                   "
+            ) #"
+
+endmacro()
+
+macro(INSTALL_PYTHON_FILES)
+    #install(
+    #PROGRAMS
+    # scripts/my_scrip.py
+    #DESTINATION
+    #${CAMKE_PYTHON_INSTALL}/${PROJECT_NAME}
+    #)
+endmacro()
 
--- setup_install.zsh
+++ setup_install.zsh
@@ -1,2 +1,3 @@
 MY_SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 CMAKE_PREFIX_PATH=${MY_SCRIPTPATH}/install:${CMAKE_PREFIX_PATH}
+PYTHONPATH=/home/beuzeboc/code/tmp/cmake_multi_repo_template/install/lib/python3.5/site-packages:${PYTHONPATH}

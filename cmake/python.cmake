85d30cfed1f47e09905a54d4e5385fc93a23ed71
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2019-01-28 14:53:21 +0100

    feat(python): install files manually

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,7 +1,8 @@
 
 macro(PREPARE_PYTHON)
     set(CMAKE_PYTHON_BUILD "${ROOT_DIR}/build/python/lib/python3.5/site-packages")
-    set(CMAKE_PYTHON_INSTALL "${ROOT_DIR}/install/lib/python3.5/site-packages")
+    #set(CMAKE_PYTHON_INSTALL "${ROOT_DIR}/install/lib/python3.5/site-packages")
+    set(CMAKE_PYTHON_INSTALL "${ROOT_DIR}/install/lib/python3/dist-packages")
     file(MAKE_DIRECTORY ${CMAKE_PYTHON_BUILD})
     file(MAKE_DIRECTORY ${CMAKE_PYTHON_INSTALL})
 endmacro()
@@ -14,15 +15,27 @@ macro(SETUP_PYTHON_PKG)
      set(PYTHON_SOURCE_PATH "${PROJECT_SOURCE_DIR}/src")
      configure_file(${ROOT_DIR}/cmake/__init__.py.in ${INIT_PKG_BUILD})
 
+     #install(CODE "
+     #set(cmd \"PYTHONPATH=${CMAKE_PYTHON_INSTALL}:$ENV{PYTHONPATH} && /usr/bin/python3 setup.py build -b ${ROOT_DIR}/build/${PROJECT_NAME} install --install-layout=deb --prefix=${ROOT_DIR}/install\")
+     #       execute_process(COMMAND bash -c \${cmd}
+     #                       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+     #                       OUTPUT_VARIABLE output
+     #                       RESULT_VARIABLE result)
+     #       message(\"\${output}!!!\")
+     #              "
+     #       ) #"
      install(CODE "
-            set(cmd \"PYTHONPATH=${CMAKE_PYTHON_INSTALL}:$ENV{PYTHONPATH} && /usr/bin/python3 ${PROJECT_SOURCE_DIR}/setup.py install --prefix=${ROOT_DIR}/install\")
-            execute_process(COMMAND bash -c \${cmd1}
-                            WORKING_DIRECTORY ${ROOT_DIR}/install
-                            OUTPUT_VARIABLE output
-                            RESULT_VARIABLE result)
-            message(\"\${output}!!!\")
-                   "
-            ) #"
+     set(PATH_TO_PY_FILES \"${CMAKE_PYTHON_INSTALL}/${PROJECT_NAME}\")
+        set(cmd \"mkdir -p \${PATH_TO_PY_FILES} &&
+            cp ${PROJECT_SOURCE_DIR}/src/${PROJECT_NAME}/*.py \${PATH_TO_PY_FILES} &&
+            touch \${PATH_TO_PY_FILES}/__init__.py\")
+        execute_process(COMMAND bash -c \${cmd}
+                        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                        OUTPUT_VARIABLE output
+                        RESULT_VARIABLE result)
+        message(\"!!!!!!!!!!!!!!!!!!! ${PROJECT_SOURCE_DIR}/src/${PROJECT_NAME}/   \")
+        message(\"\${output}!!!\")
+            ") #"
 
 endmacro()
 
--- src/a_lib/setup.py
+++ src/a_lib/setup.py
@@ -4,5 +4,5 @@ setup(
    name='a_lib',
    author_email='guillaume.beuzeboc@gmail.com',
    packages=['a_lib'],
-   package_dir = {'': '/home/beuzeboc/code/tmp/cmake_multi_repo_template/src/a_lib/src'},
+   package_dir={'': 'src'},
 )

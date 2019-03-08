2a4c81d7e4e24aeb85f152c5aff4200f3676e7b4
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2019-03-08 14:12:53 +0100

    feat(symlink): another project can use it with symlink
    now another Workspace can point to this cmakelists and use it.

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -12,7 +12,7 @@ macro(SETUP_PYTHON_PKG)
      file(MAKE_DIRECTORY ${CMAKE_PYTHON_BUILD}/${PROJECT_NAME})
      set(INIT_PKG_BUILD ${CMAKE_PYTHON_BUILD}/${PROJECT_NAME}/__init__.py)
      set(PYTHON_SOURCE_PATH "${PROJECT_SOURCE_DIR}/src")
-     configure_file(${ROOT_DIR}/cmake/__init__.py.in ${INIT_PKG_BUILD})
+     configure_file(${ORIGIN}/cmake/__init__.py.in ${INIT_PKG_BUILD})
 
      set(PATH_TO_${PROJECT_NAME}_PY_FILES "${PROJECT_SOURCE_DIR}/src/${PROJECT_NAME}")
      install(DIRECTORY ${PATH_TO_${PROJECT_NAME}_PY_FILES} DESTINATION lib/python3/dist-packages COMPONENT ${${PROJECT_NAME}_UPPER})
